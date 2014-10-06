833155fb5b4b7d0122c2ab5cf62baa1aafe115c9
Daniel Tan <melliniumorder@gmail.com>
2014-10-06 23:49:59 +0800

    remove dev

---
+++ convert_to_gh.bat
@@ -0,0 +1,19 @@
+@echo off
+rem this will point the command to the working directory of the batch file.
+cd %~dp0
+cd img
+pause
+rd /S originals
+cd ..
+cd events
+rd /S /Q 14-15
+cd ..
+rd /S /Q data
+rd /S /Q __pycache__
+rd /S /Q post
+rd /S /Q /.git
+del .gitignore
+del .gitattributes
+del *.py 
+del *.bat
+pause
