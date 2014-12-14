939ab65331d2daf4d37b7a0550a4a1d5762150a5
Daniel Tan <melliniumorder@gmail.com>
2014-12-14 10:25:10 +0800

    clean up and faq update

--- js/student.js
+++ js/student.js
@@ -8,7 +8,8 @@ function redirectUp(){
   $(".qa-li>h3").click(function(){
     var html =  "<h3>Q : "+$(this).find($(":not(i)")).html()
                 +"</h3>\n<h3>A : </h3>"+$(this).next().html()+"<br>";
-    var parent=$(this).parentsUntil(".container");
+    var parent=$(this).parentsUntil(".qa-out");
+    console.log(parent);
     $(".qa-panel-container").insertBefore(parent[parent.length-1]);
     scrollToElement($(".qa-panel"));
     $(".qa-panel").html(html);
