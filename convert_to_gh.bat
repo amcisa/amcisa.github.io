a00c7007ac550196464b686f7582f5d7deab56b0
Daniel Tan <melliniumorder@gmail.com>
2014-12-15 18:21:51 +0800

    fixed safari

--- convert_to_gh.bat
+++ convert_to_gh.bat
@@ -7,10 +7,12 @@ rd /S originals
 cd ..
 cd events
 rd /S /Q 14-15
+rd /S /Q 14-15.includes
 cd ..
 rd /S /Q data
 rd /S /Q __pycache__
 rd /S /Q post
+rd /S /Q includes
 rd /S /Q .git
 del .gitignore
 del .gitattributes
