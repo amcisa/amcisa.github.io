66358d3388cd2b1dfc42ff7f50637f6ff10d5210
Daniel Tan <melliniumorder@gmail.com>
2015-02-15 21:33:32 +0800

    update new event msd

--- sentinel.py
+++ sentinel.py
@@ -45,7 +45,7 @@ def startRender(path,batfile):
    renderer.start()
 
 if __name__ == "__main__":
-   server.serve(7000)
+   server.serve(8000)
    startRender("post","rpost.bat")
    startRender("events/14-15.post","revent.bat")
    startLogger()
