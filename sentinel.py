d25a8032e3a3bdf177c6075e5be6b10f547a9e88
Daniel Tan <melliniumorder@gmail.com>
2015-05-31 09:34:00 +0800

    Enhance sentinel.py
    - added support for includes to activate the sentinel
    - cleaned the code up and added comments

--- sentinel.py
+++ sentinel.py
@@ -4,17 +4,21 @@ import re
 import os.path
 from subprocess import Popen
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler,FileSystemEventHandler
+from watchdog.events import FileSystemEventHandler
 import server
 
 class JadeHandler(FileSystemEventHandler):
    def __init__(self):
       super().__init__()
       print("Refreshing all current versions")
+      #All templates should be completely rendered before the program
+      #can check for individual changes
       p=Popen(["jade","-P","./post","--out","./"],cwd="./",shell=True)
       p.wait(360)
+      #Search for all event year folders of format (YY-YY) 
+      #And rerenders them
       for folder in [dir for dir in os.listdir("./events") if re.search("\.post",dir)]:
-         outdir=folder.split(".")[0]
+         outdir=folder.split(".")[0] #normal format is ./events/YY-YY.post
          if not os.path.exists("./events/"+outdir): os.mkdir("./events/"+outdir)
          p=Popen(["jade","-P","./events/"+folder,"--out","./events/"+outdir],cwd="./",shell=True)
          p.wait(360)
@@ -26,33 +30,41 @@ class JadeHandler(FileSystemEventHandler):
       filedata["full-path"]=filepath
       filedata["dirname"]=os.path.dirname(filepath)
       filedata["filename"]=os.path.basename(filepath)
-      if(re.search("events.\d\d-\d\d", filepath)):
-         filedata["type"]="events"
+      filedata["type"]="render"
+      if(re.search("events.\d\d-\d\d.post", filepath)):         
          filedata["outdir"]="."+filedata["dirname"].split(".")[1]
+      elif(re.search("events.\d\d-\d\d.includes", filepath)):
+         filedata["outdir"]="."+filedata["dirname"].split(".")[1]
+         filedata["full-path"]=filedata["outdir"]+".post"
       elif(re.search("post.\w", filepath)):
-         filedata["type"]="post"
          filedata["outdir"]="./"
-      else: print(filepath)
+      elif(re.search("includes", filepath)):
+         filedata["outdir"]="./"
+         filedata["full-path"]=filedata["outdir"]+"post"
+      else: 
+         #Catch any md/jade/html files that are not post content
+         filedata["type"]="none"
       return filedata
+
    def remove_older_version(self,dirname,filename):
-      #Here I will hardcode the filepaths instead
       tempname="{0}/{1}.html".format(dirname, os.path.splitext(filename)[0])
-      #print(tempname)
-      #print(os.path.exists(tempname))
-      if(os.path.exists(tempname)):os.remove(tempname)
-      #print(os.path.exists(tempname))
+      if(os.path.exists(tempname)):
+         os.remove(tempname)
+         print("Old version found and removed: ", tempname)
+      else:
+         print("Old version not found.")
+
    def create_new_version(self,dirname,filename):
+      print("Creating new file from : \"{0}\" in : \"{1}\"".format(filename, dirname))
+      #Non blocking for parallel rendering
       p=Popen(["jade","-P",filename,"--out",dirname],cwd="./",shell=True)
-      stdout, stderr = p.communicate()
+      stdout, stderr = p.communicate() #Shows jade rendering status
       return p
+
    def generator(self,event):
-      print("File created ->"+event.src_path)
-      if event.src_path.split(".")[-1].lower() in ("jade", "md"): 
-         print("rendering")
+      if event.src_path.split(".")[-1].lower() in ("jade", "md", "html"): 
          data=self.parse_file_path(event.src_path)
-         print("parsed")
-         if(data["type"]):
-            print(data)
+         if(data["type"]!="none"):
             self.remove_older_version(data["outdir"],data["filename"])
             self.create_new_version(data["outdir"],data["full-path"])
 
@@ -67,24 +79,8 @@ def startRender():
    renderer.schedule(jade_handler,"./", recursive=True)
    renderer.start()
 
-def startLogger():
-   logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')   
-   event_handler = LoggingEventHandler()
-   observer = Observer()
-   observer.schedule(event_handler, ".", recursive=True)
-   observer.start()
-   try:
-      while True:
-         time.sleep(1)
-   except KeyboardInterrupt:
-      observer.stop()
-   observer.join()
-
 if __name__ == "__main__":
    print("Started Sentinel")
    startRender()
-   #startLogger()
    print("Launching page")
    server.serve(8000)
