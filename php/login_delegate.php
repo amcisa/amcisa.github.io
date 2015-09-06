efeb9ea04748dcdb8f8847465819169d1c0d3cb3
Daniel Tan <melliniumorder@gmail.com>
2015-09-07 00:51:21 +0800

    fake commit

--- php/login_delegate.php
+++ php/login_delegate.php
@@ -1,6 +1,6 @@
 <?php
   //login_delegate.php
-  require "/secure/checkuser.php"
+  require "/secure/checkuser.php";
   if($_POST["action"]=="checkuser"){
     echo(checkUserExists($_POST["MatricNo"]));
   }
