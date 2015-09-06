b60865b104553989980bd3292865242d9d707b96
Daniel Tan <melliniumorder@gmail.com>
2015-09-06 11:36:04 +0800

    fake commit

--- js/login.js
+++ js/login.js
@@ -7,19 +7,23 @@ $(document).ready(function(){
     var data_form= new FormData();
     data_form.append("action", "checkuser");
     data_form.append("MatricNo", $("#login-matric-no").val());
-    $.ajax({
+    delegate(data_form, function(data){
+      console.log(data);
+      $("#login-matric-no .btn").html("Go!");
+    })
+  })
+})
+
+function delegate(formdata, callback){
+  return $.ajax({
         type:"POST",
-        data:data_form,
+        data:formdata,
         url:"php/login_delegate.php",
         processData:false,
         contentType:false,
-        success: function(data){
-          console.log(data);
-          $(this).html("Go!");
-        },
+        success: callback(data),
         error:function(data){
-          console.log("failed logged in");
+          console.log("failed : ", data);
         }
       })
-  })
-})
+}
