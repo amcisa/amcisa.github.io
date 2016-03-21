ab032a92ad527c73894368eb69443b858f4b9fc8
gudgud96 <helloharry66@gmail.com>
2016-03-21 22:27:12 +0800

    amscar upload portal

--- php/amscar_upload.php
+++ php/amscar_upload.php
@@ -8,7 +8,7 @@
     
     $name = $_FILES["nominate"]["name"];
     $timestamp=date("Ymd_Gis");
-    $dir= "/home/amcisaor/public_html/gh/uploads/amscar2016/". preg_split('[_.]', $name)[0]."/".$timestamp.$name;
+    $dir= "/home/amcisaor/public_html/gh/uploads/amscar2016/". preg_split('[_.]', $name)[0]."/".$timestamp."_".$name;
     //This line below is kept for testing on computer
     //$dir = "C:/wamp/www/amcisa.github.io/uploads/amscar2016/" . preg_split('[_.]', $name)[0]."/".$name;
     if (move_uploaded_file($_FILES["nominate"]["tmp_name"], $dir)) {
