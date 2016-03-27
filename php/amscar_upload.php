cbf26042c0e655e8ad2cbcb18ee8582afa7aeee2
gudgud96 <helloharry66@gmail.com>
2016-03-27 18:02:31 +0800

    amscar

--- php/amscar_upload.php
+++ php/amscar_upload.php
@@ -9,12 +9,12 @@
     $name = $_FILES["nominate"]["name"];
     $timestamp=date("Ymd_Gis");
     $dir= "/home/amcisaor/public_html/gh/uploads/amscar2016/". preg_split('[_.]', $name)[0]."/".$timestamp.'_'.$name;
-    print_r($_FILES);
+    print_r($_FILES["caption"]);
     //This line below is kept for testing on computer
     //$dir = "C:/wamp/www/amcisa.github.io/uploads/amscar2016/" . preg_split('[_.]', $name)[0]."/".$name;
     if (move_uploaded_file($_FILES["nominate"]["tmp_name"], $dir)) {
       $headers = "Content-Type: text/html; charset=UTF-8";
-      mail('TANH0207@e.ntu.edu.sg','AmScar2016 Nomination', $_FILES["nominate"]["name"], $headers);
+      //mail('TANH0207@e.ntu.edu.sg','AmScar2016 Nomination', $_FILES["nominate"]["name"], $headers);
       echo 0;
     }
       
