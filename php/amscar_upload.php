ee0a9477500f2f63c7da6b31be262c73f6367738
gudgud96 <helloharry66@gmail.com>
2016-03-27 17:31:16 +0800

    amscar bug fix

--- php/amscar_upload.php
+++ php/amscar_upload.php
@@ -11,7 +11,8 @@
     //This line below is kept for testing on computer
     //$dir = "C:/wamp/www/amcisa.github.io/uploads/amscar2016/" . preg_split('[_.]', $name)[0]."/".$name;
     if (move_uploaded_file($_FILES["nominate"]["tmp_name"], $dir)) {
-      mail('TANH0207@e.ntu.edu.sg','AmScar2016 Nomination', $_FILES["nominate"]["name"]);
+      $headers = "Content-Type: text/html; charset=UTF-8"
+      mail('TANH0207@e.ntu.edu.sg','AmScar2016 Nomination', $_FILES["nominate"]["name"], $headers);
       echo 0;
     }
       
